284d06bdb13e23f8eda8db71914f2c0561dd21f4
Rahul Sarma <rahulsarma7007@gmail.com>
2023-11-23 20:03:12 +0530

    basic app added

---
+++ bazaar/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+});
+
+export default App;
